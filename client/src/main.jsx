165b5ffe0b2d496b355c78bcda3b95b16b5bc148
DevMirza <pzhafeez@gmail.com>
2022-12-14 12:49:47 +0000

    sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './index.css'
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mainnet;
