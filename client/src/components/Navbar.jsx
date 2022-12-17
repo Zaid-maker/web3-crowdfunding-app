e29fd14c6bad12bdd904e0431951851a1fd07266
DevMirza <pzhafeez@gmail.com>
2022-12-17 14:30:21 +0000

    complete navbar#

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-//import { useStateContext } from '../context';
-import { CustomButton } from './';
+import { useStateContext } from "../context";
+import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
+  const { connect, address } = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input
           type="text"
-          placeholder="search for campaigns"
+          placeholder="Search for campaigns"
           className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
         />
 
@@ -31,24 +33,33 @@ const Navbar = () => {
       <div className="sm:flex hidden flex-row justify-end gap-4">
         <CustomButton
           btnType="button"
-          title={'Create a campaign'}
-          styles={'bg-[#1dc071]'}
+          title={address ? "Create a campaign" : "Connect"}
+          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+          handleClick={() => {
+            if (address) navigate("create-campaign");
+            else connect();
+          }}
         />
 
         <Link to="/profile">
           <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img
               src={thirdweb}
-              alt="thirdweb"
+              alt="user"
               className="w-[60%] h-[60%] object-contain"
             />
           </div>
         </Link>
       </div>
 
+      {/* Small screen navigation */}
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-          <img src={logo} alt="" className="w-[60%] h-[60%] object-contain" />
+          <img
+            src={logo}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
         </div>
 
         <img
@@ -70,6 +81,11 @@ const Navbar = () => {
                 className={`flex p-4 ${
                   isActive === link.name && "bg-[#3a3a43]"
                 }`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
               >
                 <img
                   src={link.imgUrl}
@@ -92,8 +108,12 @@ const Navbar = () => {
           <div className="flex mx-4">
             <CustomButton
               btnType="button"
-              title={'Create a campaign'}
-              styles={'bg-[#1dc071]'}
+              title={address ? "Create a campaign" : "Connect"}
+              styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+              handleClick={() => {
+                if (address) navigate("create-campaign");
+                else connect();
+              }}
             />
           </div>
         </div>
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,13 +1,25 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext } from "react";
 
-const StateContext = createContext()
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
+
+const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    return (
-        <StateContext.Provider value={{}}>
-            {children}
-        </StateContext.Provider>
-    )
-}
+  const address = useAddress();
+  const connect = useMetamask();
+
+  return (
+    <StateContext.Provider value={{ address, connect }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
 
 export const useStateContext = () => useContext(StateContext);
