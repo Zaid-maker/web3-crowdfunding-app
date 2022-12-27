eede7facaa314ac4ba4e71d65053ee6cf0238ebb
Zaid-maker <pzhafeez@gmail.com>
2022-12-27 15:42:12 +0500

    get campiagns context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -12,11 +12,31 @@ import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract('')
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign')
+
   const address = useAddress();
   const connect = useMetamask();
 
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns')
+
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pid: i
+    }))
+
+    return parsedCampaigns;
+  }
+
   return (
-    <StateContext.Provider value={{ address, connect }}>
+    <StateContext.Provider value={{ address, contract ,connect, getCampaigns }}>
       {children}
     </StateContext.Provider>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 import { DisplayCampaigns } from '../components'
+import { useStateContext } from '../context'
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     const [campaigns, setCampaigns] = useState([])
+
+    const { address, contract, getCampaigns } = useStateContext()
+
+    const fetchCampaigns = async () => {
+      setIsLoading(true);
+      const data = await getCampaigns()
+      setCampaigns(data)
+      setIsLoading(false)
+    }
+
+    useEffect(() => {
+      if(contract) fetchCampaigns()
+    }, [address, contract])
 
     return (
         <DisplayCampaigns
