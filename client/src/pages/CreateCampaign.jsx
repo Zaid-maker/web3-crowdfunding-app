682639805be8de7f4dbdaa14782ef7798889400c
Zaid-maker <pzhafeez@gmail.com>
2022-12-28 19:30:05 +0500

    complete create campaign

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -4,7 +4,7 @@ import { ethers } from 'ethers';
 
 import { useStateContext } from '../context';
 import { money } from '../assets';
-import { FormField, Loader } from '../components';
+import { FormField, CustomButton, Loader } from '../components';
 import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
@@ -59,6 +59,47 @@ const CreateCampaign = () => {
                     value={form.description}
                     handleChange={(e) => handleFormFieldChange('description', e)}
                 />
+
+                <div className='w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]'>
+                    <img src={money} alt="money" className='w-[40px] h-[40px] object-contain' />
+                    <h4 className='font-epilogue font-bold text-[25px] text-white ml-[20px]'>
+                        You will get 100% of the raised amount
+                    </h4>
+                </div>
+
+                <div className='flex flex-wrap gap-[40px]'>
+                    <FormField
+                        labelName="Goal *"
+                        placeholder="ETH 0.50*"
+                        inputType="text"
+                        value={form.target}
+                        handleChange={(e) => handleFormFieldChange('target', e)}
+                    />
+
+                    <FormField
+                        labelName="End Date *"
+                        placeholder="End Date*"
+                        inputType="date"
+                        value={form.deadline}
+                        handleChange={(e) => handleFormFieldChange('deadline', e)}
+                    />
+                </div>
+
+                <FormField
+                    labelName="Campaign Image *"
+                    placeholder="Place image URL of your campaign"
+                    inputType="url"
+                    value={form.image}
+                    handleChange={(e) => handleFormFieldChange('image', e)}
+                />
+
+                <div className='flex justify-center items-center mt-[40px]'>
+                    <CustomButton
+                        btnType="submit"
+                        title="Submit new campaign"
+                        styles="bg-[#1dc071]"
+                    />
+                </div>
             </form>
         </div>
     )
