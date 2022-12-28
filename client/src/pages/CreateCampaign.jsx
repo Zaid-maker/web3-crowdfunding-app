fe3bb0e164cacc54c39de3f1b16b83c6733e4fbc
Zaid-maker <pzhafeez@gmail.com>
2022-12-28 19:53:55 +0500

    fix

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -3,18 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 
 import { useStateContext } from '../context';
-import { createCampaign, money } from '../assets';
+import { money } from '../assets';
 import { FormField, CustomButton, Loader } from '../components';
 import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
+    const { createCampaign } = useStateContext();
     const [form, setForm] = useState({
         name: '',
         title: '',
         description: '',
-        target: '',
+        target: '', 
         deadline: '',
         image: ''
     });
