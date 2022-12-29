f40d9b9f23a4545e61f392c46f29c41fdbb9148b
Zaid-maker <pzhafeez@gmail.com>
2022-12-29 13:55:33 +0500

    create campaign context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -12,16 +12,33 @@ import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9')
-  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign')
+  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
   const connect = useMetamask();
 
-  const getCampaigns = async () => {
-    const campaigns = await contract.call('getCampaigns')
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, // owner
+        form.title, // title
+        form.description, // description
+        form.target,
+        new Date(form.deadline).getTime(), // deadline,
+        form.image
+      ])
 
-    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      console.log('contract call success', data)
+    } catch (error) {
+      console.log('contract call failure', error)
+    }
+  }
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
       owner: campaign.owner,
       title: campaign.title,
       description: campaign.description,
@@ -29,14 +46,20 @@ export const StateContextProvider = ({ children }) => {
       deadline: campaign.deadline.toNumber(),
       amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
       image: campaign.image,
-      pid: i
-    }))
+      pId: i
+    }));
 
-    return parsedCampaigns;
+    return parsedCampaings;
   }
 
   return (
-    <StateContext.Provider value={{ address, contract ,connect, getCampaigns }}>
+    <StateContext.Provider value={{
+      address,
+      contract, 
+      connect,
+      createCampaign: publishCampaign,
+      getCampaigns
+    }}>
       {children}
     </StateContext.Provider>
   );
