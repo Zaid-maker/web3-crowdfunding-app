bd1b6b23f2c79fb2d3be0f1e852bd9d3237d70ba
Zaid-maker <pzhafeez@gmail.com>
2023-01-02 20:13:14 +0500

    campaign details and countbox

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+
+import { useStateContext } from "../context";
+import { CountBox, Loader } from "../components";
+import { calculateBarPercentage, daysLeft } from "../utils";
+import { thirdweb } from "../assets";
 
 const CampaignDetails = () => {
-  return (
-    <div>CampaignDetails</div>
-  )
-}
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const { donate, getDonations, contract, address } = useStateContext();
 
-export default CampaignDetails
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState("");
+  const [donators, setDonators] = useState([]);
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaign"
+            className="w-full h-[410px] object-cover rounded-xl"
+          />
+
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            <div
+              className="absolute h-full bg-[#4acd8d]"
+              style={{
+                width: `${calculateBarPercentage(
+                  state.target,
+                  state.amountCollected
+                )}%`,
+                maxWidt: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox />
+          <CountBox />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CampaignDetails;
