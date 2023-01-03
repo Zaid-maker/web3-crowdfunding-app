d141b69f12302955a84bbcc61a3a7898308fc8bd
Zaid-maker <pzhafeez@gmail.com>
2023-01-03 23:00:47 +0500

    complete the fund card

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -13,6 +13,8 @@ const FundCard = ({
   image,
   handleClick,
 }) => {
+  const remainingDays = daysLeft(deadline);
+
   return (
     <div
       className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer"
@@ -42,6 +44,38 @@ const FundCard = ({
           </h3>
           <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">
             {description}
+          </p>
+        </div>
+
+        <div className="flex justify-between flex-wrap mt-[15px] gap-2">
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+              {amountCollected}
+            </h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+              Raised of {target}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+              {remainingDays}
+            </h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+              Days Left
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center mt-[20px] gap-[12px]">
+          <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
+            <img
+              src={thirdweb}
+              alt="user"
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
+          <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">
+            by <span className="text-[#b2b3bd]">{owner}</span>
           </p>
         </div>
       </div>
